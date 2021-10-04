a90f71c2731e97b7047ea1e14dbcea7e2d850f85
Diego <alejorvela@gmail.com>
2021-10-04 16:16:48 -0500

    fluent validations added

--- TiendaServicios.Api.Autor/Startup.cs
+++ TiendaServicios.Api.Autor/Startup.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,11 +29,13 @@ namespace TiendaServicios.Api.Autor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<Nuevo>());
+
             services.AddDbContext<ContextoAutor>(options =>
             {
                 options.UseNpgsql(Configuration.GetConnectionString("ConexionDatabase"));
             });
+
             services.AddMediatR(typeof(Nuevo.Manejador).Assembly);
         }
 
