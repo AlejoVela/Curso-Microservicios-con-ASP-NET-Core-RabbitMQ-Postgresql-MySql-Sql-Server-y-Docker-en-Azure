070a3dbe9596eeb46846b7eced3c338f150cf91d
Diego <alejorvela@gmail.com>
2021-10-04 15:59:28 -0500

    added get, get by AutorID and post without fluent and DTO

---
+++ TiendaServicios.Api.Autor/Aplicacion/Consulta.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Autor.Modelo;
+using TiendaServicios.Api.Autor.Persistencia;
+
+namespace TiendaServicios.Api.Autor.Aplicación
+{
+    public class Consulta
+    {
+        public class ListaAutor : IRequest<List<AutorLibro>>
+        {
+
+        }
+        public class Manejador : IRequestHandler<ListaAutor, List<AutorLibro>>
+        {
+            private readonly ContextoAutor _context;
+            public Manejador(ContextoAutor context)
+            {
+                this._context = context;
+            }
+            public async Task<List<AutorLibro>> Handle(ListaAutor request, CancellationToken cancellationToken)
+            {
+                var autores = await _context.AutorLibro.ToListAsync();
+
+                return autores;
+            }
+        }
+    }
+}
