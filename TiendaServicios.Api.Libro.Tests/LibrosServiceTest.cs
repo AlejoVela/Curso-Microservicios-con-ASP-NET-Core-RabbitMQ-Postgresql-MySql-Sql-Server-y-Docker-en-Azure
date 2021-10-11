f7d208f60d4002848a11e7c3af54921c165db0da
Diego <alejorvela@gmail.com>
2021-10-11 00:01:22 -0500

    first test to booksService

--- TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -1,9 +1,13 @@
 ﻿using AutoMapper;
+using GenFu;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using TiendaServicios.Api.Libro.Aplicacion;
+using TiendaServicios.Api.Libro.Modelo;
 using TiendaServicios.Api.Libro.Persistencia;
 using Xunit;
 
@@ -11,21 +15,68 @@ namespace TiendaServicios.Api.Libro.Tests
 {
     public class LibrosServiceTest
     {
-        [Fact]
-        public void GetLibros()
+        private IEnumerable<LibreriaMaterial> ObtenerDataPrueba()
         {
+            A.Configure<LibreriaMaterial>()
+                .Fill(x => x.Titulo).AsArticleTitle()
+                .Fill(x => x.LibreriaMaterialId, () => { return Guid.NewGuid(); });
+
+            //le pasamos la cantidad de elementos que queremos que nos genere
+            var lista = A.ListOf<LibreriaMaterial>(30);
+            //esto ultimo lo hacemos para colocar un cero en el id del primer elemento de la lista
+            //esto con motivo de poder usar este elemento en otras pruebas con su id "0"
+            lista[0].LibreriaMaterialId = Guid.Empty;
+
+            return lista;
+        }
+
+        private Mock<ContextoLibreria> CrearContexto()
+        {
+            var dataPrueba = ObtenerDataPrueba().AsQueryable();
+
+            var dbSet = new Mock<DbSet<LibreriaMaterial>>();
+            //Con el siguiente codigo, indicamos que la clase se debe manejar como una clase de tipo entidad
+            //al no estar trabajando con una instancia de sql, debemos hacer esto manualmente
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Provider).Returns(dataPrueba.Provider);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Expression).Returns(dataPrueba.Expression);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.ElementType).Returns(dataPrueba.ElementType);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.GetEnumerator()).Returns(dataPrueba.GetEnumerator());
+
+            dbSet.As<IAsyncEnumerable<LibreriaMaterial>>().Setup(x => x.GetAsyncEnumerator(new System.Threading.CancellationToken()))
+                    .Returns(new AsyncEnumerator<LibreriaMaterial>(dataPrueba.GetEnumerator()));
+
+            var contexto = new Mock<ContextoLibreria>();
+            contexto.Setup(x => x.LibreriaMaterial).Returns(dbSet.Object);
+            return contexto;
+        }
+
+        [Fact]
+        public async void GetLibros()
+        {
+            System.Diagnostics.Debugger.Launch();
             //Debemos emular a los objetos inyectados dentro de nuestras clases a probar
             //en este caso, probaremos la clase nuevo-manejador del proyecto libro
             //1. Emular a la instancia de EntityFramework core - ContextoLibreria
             //para emular las acciones y eventos de un objeto de ambiente de unit test
             //utilizamos objetos de tipo Mock
-            var mockContexto = new Mock<ContextoLibreria>();
+            var mockContexto = CrearContexto();
 
             //2. Emulamos tampien a un objeto IMapper
-            var mockMapper = new Mock<IMapper>();
+            var mapConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingTest());
+            });
+
+            var mapper = mapConfig.CreateMapper();
 
             //3. Intanciar a la clase manejador y pasar como parametros los mocks creados
-            Consulta.Manejador manejador = new Consulta.Manejador(mockContexto.Object, mockMapper.Object);
+            Consulta.Manejador manejador = new Consulta.Manejador(mockContexto.Object, mapper);
+
+            Consulta.Ejecuta request = new Consulta.Ejecuta();
+
+            var lista = await manejador.Handle(request, new System.Threading.CancellationToken());
+
+            Assert.True(lista.Any());
         }
     }
 }
